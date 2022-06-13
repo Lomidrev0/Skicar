00303e7a1c14ca5c9ed9a171a4414584b134c891
Lomidrev0 <knutdamian106@gmail.com>
2022-06-13 23:24:35 +0200

    local history

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,25 +4,21 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 import javax.imageio.ImageIO;
-import java.awt.event.MouseMotionListener;
 import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
 
 public class Controller {
     @FXML
@@ -57,14 +53,23 @@ public class Controller {
     private Button zo;
     @FXML
     private Button zi;
+    @FXML
+    private ImageView redo;
+    @FXML
+    private ImageView undo;
 
 
     public boolean pen = true;
+    public static int history=0;
+
 
 
     public void initialize() {
-
-
+        Image i=new Image(System.getProperty("user.dir")+"\\src\\res\\icons8-undo-60.png");
+        Image i0=new Image(System.getProperty("user.dir")+"\\src\\res\\icons8-redo-60.png");
+        undo.setImage(i);
+        redo.setImage(i0);
+        GraphicsContext g = canvas.getGraphicsContext2D();
         ObservableList<String> options = FXCollections.observableArrayList("Pen", "Highlighter", "spray", "crayon", "Text Writer");
         comboBox.setItems(options);
         ObservableList<String> options2 = FXCollections.observableArrayList("Line", "Circle", "Elipse", "square");
@@ -72,10 +77,6 @@ public class Controller {
         ObservableList<String> options3 = FXCollections.observableArrayList("Square pen", "Circle pen");
         comboBox3.setItems(options3);
 
-
-        GraphicsContext g = canvas.getGraphicsContext2D();
-        // g.setFill(Color.BLACK);
-        //g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
         if (pen == true) {
             canvas.setOnMouseDragged(e -> {
                 double size = Double.parseDouble(brushSize.getText());
@@ -98,15 +99,16 @@ public class Controller {
                 double x = e.getX() - size / 2;
                 double y = e.getY() - size / 2;
 
-
                 if (eraser.isSelected()) {
                     g.clearRect(x, y, size, size);
+                    history();
                 } else {
                     g.setFill(colorPicker.getValue());
                     g.fillRect(x, y, size, size);
+                    history();
                 }
-
             });
+            history();
         } else {
             canvas.setOnMouseDragged(e -> {
                 double size = Double.parseDouble(brushSize.getText());
@@ -115,7 +117,6 @@ public class Controller {
 
                 if (eraser.isSelected()) {
                     g.clearRect(x, y, size, size);
-
                 } else {
                     g.setFill(colorPicker.getValue());
                     g.fillOval(x, y, size, size);
@@ -130,18 +131,15 @@ public class Controller {
                 double x = e.getX() - size / 2;
                 double y = e.getY() - size / 2;
 
-
                 if (eraser.isSelected()) {
                     g.clearRect(x, y, size, size);
                 } else {
                     g.setFill(colorPicker.getValue());
                     g.fillOval(x, y, size, size);
                 }
-
             });
+            history();
         }
-
-
     }
 
     public void pen() {
@@ -155,8 +153,6 @@ public class Controller {
             initialize();
             //System.out.println(comboBox3.getValue());
         }
-
-
     }
    /** public void zoomIn(){
         canvas.setScaleX(2);
@@ -179,28 +175,79 @@ public class Controller {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Canvas size error");
             alert.setContentText("Looks like you've exceeded the max width or height of sheet!");
-
             alert.showAndWait();
         }
     }
 
-
     public void save(){
-        savem();
+        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try {
             WritableImage snapshot =canvas.snapshot(null,null);
-            ImageIO.write(SwingFXUtils.fromFXImage(snapshot,null),"png",new File("Paint.png"));
+            ImageIO.write(SwingFXUtils.fromFXImage(snapshot,null),"png",new File(System.getProperty("user.dir")+"\\src\\sample\\saved\\Paint.png"));
 
         }catch (Exception e){
             System.out.println("Failed to save image"+e);
         }
     }
+    public void history(){
+        history++;
+        try {
+            if (history<=20){
+                WritableImage snapshot =canvas.snapshot(null,null);
+                ImageIO.write(SwingFXUtils.fromFXImage(snapshot,null),"png",new File(System.getProperty("user.dir")+"\\src\\sample\\saved\\h\\Paint"+history+".png"));
+            }
+            else {
+                try {
+                    Files.deleteIfExists(
+                            Paths.get(System.getProperty("user.dir")+"\\src\\sample\\saved\\h\\Paint"+(history-20)+".png"));
+                }
+                catch (NoSuchFileException e) {
+                    System.out.println("No such file/directory exists");
+                }
+                WritableImage snapshot =canvas.snapshot(null,null);
+                ImageIO.write(SwingFXUtils.fromFXImage(snapshot,null),"png",new File(System.getProperty("user.dir")+"\\src\\sample\\saved\\h\\Paint"+history+".png"));
+            }
+        }catch (Exception e){
+            System.out.println("Failed to save image"+e);
+        }
+    }
+
+    public void load(){
+        GraphicsContext g = canvas.getGraphicsContext2D();
+        Image image= new Image(getClass().getResourceAsStream("Paint.png"));
+        g.drawImage(image,0,0,600,600);
+    }
 
     public void exit(){
+        for (int i = 0; i <20 ; i++) {
+            try {
+                Files.deleteIfExists(
+                        Paths.get(System.getProperty("user.dir")+"\\src\\sample\\saved\\h\\Paint"+(history-i)+".png"));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         Platform.exit();
     }
 
+    public static void clearH(){
+        for (int i = 0; i <20 ; i++) {
+            try {
+                Files.deleteIfExists(
+                        Paths.get(System.getProperty("user.dir")+"\\src\\sample\\saved\\h\\Paint"+(history-i)+".png"));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void undo(){
 
+    }
+    public void redo(){
+
+    }
 
     public void mouseMoved(javafx.scene.input.MouseEvent mouseEvent ) {
 
@@ -209,13 +256,4 @@ public class Controller {
             lx.setText("X: "+x[0]);
             ly.setText("Y: "+y[0]);
     }
-    Paint dd;
-    public void savem(){
-        GraphicsContext g = canvas.getGraphicsContext2D();
-        //System.out.println(g.fil);
-
-    }
-
-
-
 }
